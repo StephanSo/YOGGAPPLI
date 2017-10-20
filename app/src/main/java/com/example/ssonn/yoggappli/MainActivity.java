f2f2d49339467fc9c2455875d6137dfb0e0f42fc
StephanSo <s.sonnois@gmail.com>
2017-10-20 09:39:25 +0200

    Lien entre Voir Pref/Fic/Sqlite et l'activity Results

--- app/src/main/java/com/example/ssonn/yoggappli/MainActivity.java
+++ app/src/main/java/com/example/ssonn/yoggappli/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ssonn.yoggappli;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnVoirPrefs =(Button) findViewById(R.id.btnVoirPrefs);
+        btnVoirPrefs.setOnClickListener(observateurClickBoutonVoir);
+        Button btnVoirFiles = (Button) findViewById(R.id.btnVoirFile);
+        btnVoirFiles.setOnClickListener(observateurClickBoutonVoir);
+        Button btnVoirSQLite = (Button) findViewById(R.id.btnVoirSQLite);
+        btnVoirSQLite.setOnClickListener(observateurClickBoutonVoir);
     }
+
+    private View.OnClickListener observateurClickBoutonVoir = new View.OnClickListener(){
+        public void onClick(View v){
+            switch (v.getId()){
+                case R.id.btnVoirPrefs:
+                    Intent i = new Intent(getApplication(), Results.class);
+                    i.putExtra("ChoixMethode","Préferences");
+                    startActivity(i);
+                    break;
+                case R.id.btnVoirFile:
+                    Intent j = new Intent(getApplication(),Results.class);
+                    j.putExtra("ChoixMethode", "Fichier");
+                    startActivity(j);
+                    break;
+                case R.id.btnVoirSQLite:
+                    Intent y = new Intent(getApplication(),Results.class);
+                    y.putExtra("ChoixMethode","SQLite");
+                    startActivity(y);
+                    break;
+
+            }
+        }
+    };
 }
--- app/src/main/java/com/example/ssonn/yoggappli/Results.java
+++ app/src/main/java/com/example/ssonn/yoggappli/Results.java
@@ -1,7 +1,9 @@
 package com.example.ssonn.yoggappli;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        Bundle b = getIntent().getExtras();
+        String choix = b.getString("ChoixMethode");
+        TextView bienvenue = (TextView) findViewById(R.id.msgBienvenue);
+        bienvenue.setText("Bienvenue dans voir "+choix);
     }
 }
